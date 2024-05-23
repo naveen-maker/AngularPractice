3d0a2710a5531d024fc944a754558e3aca214580
Naveen.Kumarnd <Naveen.Kumarnd@halliburton.com>
2024-05-23 19:15:31 +0530

    spread

--- Code/WebClient/Practice/src/app/app.component.ts
+++ Code/WebClient/Practice/src/app/app.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.less'
 })
-export class AppComponent {
-  title = 'Practice';
+export class AppComponent implements OnInit{
+
+  constructor(){
+
+  }
+
+  ngOnInit(): void {
+    
+    this.spread();
+
+  }
+
+  public spread(){
+    const arr1 = [1,2,3];
+    const arr2 = [4,5,6];
+    const arr3 = [...arr1,...arr2];
+
+    console.log('spread ',arr3);
+
+  }
+
 }
+
+
